a4d07959b2a0a74fb6c8146bd3c939fe9909667f
gengyu <476413818@qq.com>
2025-07-16 16:47:32 +0800

    Update __main__.py

--- src/whisper_subtitle/__main__.py
+++ src/whisper_subtitle/__main__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Main entry point for the whisper_subtitle package."""
 
-from whisper_subtitle.cli.main import cli
+from .cli.main import cli
 
+
+# PYTHONPATH=src python3 -m whisper_subtitle.api.main
 if __name__ == '__main__':
     cli()
